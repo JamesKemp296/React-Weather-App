ade9d3dd1b620316f784cbc11e271a5d83a0c03b
JamesKemp296 <jameskemp2796@yahoo.com>
2019-12-29 19:32:45 -0500

    Created weather card with JSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+
+import WeatherCard from './components/WeatherCard/component'
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <WeatherCard />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/WeatherCard/Icon.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const Icon = props => {
+  const Icon = styled.img`
+    width: 40%;
+  `
+
+  return <Icon src="" alt="weather icon" />
+}
+
+export default Icon
