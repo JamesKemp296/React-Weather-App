9ff2209f75487044b77a12e562eec2bb431670f7
JamesKemp296 <jameskemp2796@yahoo.com>
2020-01-04 16:55:23 -0500

    initial fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
-
 import WeatherCard from './components/WeatherCard/component'
 import './App.css'
 
 function App() {
+  const data = async () => {
+    const apiRes = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`
+    )
+    const resJSON = await apiRes.json()
+    return resJSON
+  }
+  data().then(res => {
+    console.log(res.main)
+  })
+
   return (
     <div className="App">
       <WeatherCard temp={30} condition="Clear" city="Sydney" country="AU" />
