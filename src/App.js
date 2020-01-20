823e944de5b0a2ec39e33b44a8b981898fa4d47e
JamesKemp296 <jameskemp2796@yahoo.com>
2020-01-20 11:31:36 -0500

    added unit change and fetching info from api in card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import WeatherCard from './components/WeatherCard/component'
 import './App.css'
 
 function App() {
-  const data = async () => {
+  const [query, setQuery] = useState('london, gb')
+  const [city, setCity] = useState('')
+  const [unit, setUnit] = useState('metric')
+  const [temp, setTemp] = useState('')
+  const [condition, setCondition] = useState('')
+  const [country, setCountry] = useState('')
+
+  const data = async q => {
     const apiRes = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`
+      `http://api.openweathermap.org/data/2.5/weather?q=${q}&units=${unit}&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`
     )
     const resJSON = await apiRes.json()
     return resJSON
   }
-  data().then(res => {
-    console.log(res.main)
-  })
-
+  const handleSearch = e => {
+    e.preventDefault()
+    data(query).then(res => {
+      setTemp(Math.floor(res.main.temp))
+      setCondition(res.weather[0].main)
+      setCountry(res.sys.country)
+      setCity(res.name)
+    })
+  }
+  const handleUnitChange = () => {
+    unit === 'imperial' ? setUnit('metric') : setUnit('imperial')
+    let unitTemp = ''
+    if (unit === 'imperial') {
+      unitTemp = ((temp - 32) * 5) / 9
+      setTemp(Math.floor(unitTemp))
+    } else {
+      unitTemp = (temp * 9) / 5 + 32
+      setTemp(Math.floor(unitTemp))
+    }
+  }
   return (
     <div className="App">
-      <WeatherCard temp={30} condition="Clear" city="Sydney" country="AU" />
-      <WeatherCard temp={-5} condition="Clouds" city="New York" country="USA" />
-      <WeatherCard temp={40} condition="Tornado" city="Miami" country="USA" />
+      <WeatherCard
+        unit={unit}
+        handleUnitChange={handleUnitChange}
+        setUnit={setUnit}
+        temp={temp}
+        condition={condition}
+        city={city}
+        country={country}
+      />
+      <form onSubmit={e => handleSearch(e)}>
+        <input value={query} onChange={e => setQuery(e.target.value)} />
+        <button type="submit">Search</button>
+      </form>
     </div>
   )
 }
