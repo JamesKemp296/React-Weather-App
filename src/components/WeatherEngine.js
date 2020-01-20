77e51e79c0224ea4ddd625ca813f7559fa373dcf
JamesKemp296 <jameskemp2796@yahoo.com>
2020-01-20 15:14:35 -0500

    try and catch, loading and error handling

--- src/components/WeatherEngine.js
+++ src/components/WeatherEngine.js
@@ -4,6 +4,8 @@ import WeatherCard from './WeatherCard/component'
 
 const WeatherEngine = ({ location }) => {
   const [query, setQuery] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
   const [unit, setUnit] = useState('metric')
   const [weather, setWeather] = useState({
     ctemp: null,
@@ -15,45 +17,67 @@ const WeatherEngine = ({ location }) => {
   const { ctemp, ftemp, condition, country, city } = weather
 
   const getWeather = async q => {
-    const apiRes = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${q}&units=${unit}&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`
-    )
-    const resJSON = await apiRes.json()
-    setWeather({
-      ftemp: Math.round((resJSON.main.temp * 9) / 5 + 32),
-      ctemp: Math.round(resJSON.main.temp),
-      city: resJSON.name,
-      condition: resJSON.weather[0].main,
-      country: resJSON.sys.country
-    })
+    setQuery('')
+    setLoading(true)
+    try {
+      const apiRes = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${q}&units=${unit}&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`
+      )
+      const resJSON = await apiRes.json()
+      setWeather({
+        ftemp: Math.round((resJSON.main.temp * 9) / 5 + 32),
+        ctemp: Math.round(resJSON.main.temp),
+        city: resJSON.name,
+        condition: resJSON.weather[0].main,
+        country: resJSON.sys.country
+      })
+    } catch (error) {
+      setError(true)
+    }
+    setLoading(false)
   }
+
   const handleSearch = e => {
     e.preventDefault()
     getWeather(query)
   }
+
   useEffect(() => {
     getWeather(location)
   }, [location])
+
   const handleUnitChange = () => {
     unit === 'imperial' ? setUnit('metric') : setUnit('imperial')
   }
 
   return (
     <div>
-      <WeatherCard
-        unit={unit}
-        handleUnitChange={handleUnitChange}
-        setUnit={setUnit}
-        ctemp={ctemp}
-        ftemp={ftemp}
-        condition={condition}
-        city={city}
-        country={country}
-      />
-      <form onSubmit={e => handleSearch(e)}>
-        <input value={query} onChange={e => setQuery(e.target.value)} />
-        <button type="submit">Search</button>
-      </form>
+      {!loading && !error ? (
+        <div>
+          <WeatherCard
+            unit={unit}
+            handleUnitChange={handleUnitChange}
+            setUnit={setUnit}
+            ctemp={ctemp}
+            ftemp={ftemp}
+            condition={condition}
+            city={city}
+            country={country}
+          />
+          <form onSubmit={e => handleSearch(e)}>
+            <input value={query} onChange={e => setQuery(e.target.value)} />
+            <button type="submit">Search</button>
+          </form>
+        </div>
+      ) : loading ? (
+        <div style={{ color: 'black' }}>Loading</div>
+      ) : !loading && error ? (
+        <div style={{ color: 'black' }}>
+          There has been an error
+          <br />
+          <button onClick={() => setError(false)}>Reset</button>
+        </div>
+      ) : null}
     </div>
   )
 }
